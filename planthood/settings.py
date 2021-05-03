4c88525795eae40aebad573d29aa307758b4c9d1
megaz0rd <akropiewnicki@gmail.com>
2021-05-03 22:01:10 +0200

    heroku fix

--- planthood/settings.py
+++ planthood/settings.py
@@ -119,11 +119,11 @@ MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
-STATIC_URL = "/static/"
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATIC_URL = "/static/"
+#
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 # Email settings
 EMAIL_HOST = 'smtp.gmail.com'  # 'localhost'
