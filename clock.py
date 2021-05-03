678049bb49b46936fd4dddf77adba2808e0a5663
megaz0rd <akropiewnicki@gmail.com>
2021-05-03 21:53:16 +0200

    heroku settings

--- clock.py
+++ clock.py
@@ -3,14 +3,9 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
-    print('This job is run every three minutes.')
-
-
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
+@sched.scheduled_job('cron', day_of_week='mon-sat', hour=8)
 def scheduled_job():
-    print('This job is run every weekday at 5pm.')
+    print('This job is run every weekday at 8am.')
 
 
 sched.start()
