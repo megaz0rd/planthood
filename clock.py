db65ad2150690494cab4b84751af420ee31b7ada
megaz0rd <akropiewnicki@gmail.com>
2021-05-04 19:09:20 +0200

    heroku tests

--- clock.py
+++ clock.py
@@ -1,23 +1,26 @@
 import django
 from django.conf import settings
+from planthood import settings as planthood_defaults
+
+settings.configure(default_settings=planthood_defaults, DEBUG=True)
+django.setup()
+
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from django.core.mail import EmailMessage
-
-settings.configure()
-django.setup()
+from django.core.mail import send_mail
 
 sched = BlockingScheduler()
 
 
 @sched.scheduled_job('interval', minutes=3)
 def scheduled_job():
-    email = EmailMessage(
+    send_mail(
         'test tematu',
         'test treści',
-        to=['planthood-mokotow@gmail.com']
+        'planthood-mokotow@gmail.com',
+        ['planthood-mokotow@gmail.com'],
+        fail_silently=False
     )
-    email.send()
     print('Reminders sent at 8am.')
 
 
