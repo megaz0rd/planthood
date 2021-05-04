d0127630af9c08391d4933ac6a5ef7f5507071a9
megaz0rd <akropiewnicki@gmail.com>
2021-05-04 15:28:52 +0200

    heroku settings

--- planthood/settings.py
+++ planthood/settings.py
@@ -127,7 +127,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # 'localhost'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'planthood.mokotow@gmail.com'
 EMAIL_HOST_PASSWORD = 'dkzkncjxqsrfrchc'
