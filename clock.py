c19c85ef6556d2fcde47abfac3dde9f493dca9ba
megaz0rd <akropiewnicki@gmail.com>
2021-05-04 22:32:27 +0200

    Created working job for APScheduler

--- clock.py
+++ clock.py
@@ -3,25 +3,30 @@ import django
 from django.core import mail
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-from plantswap.models import Reminder
-from plantswap_api.utils import today
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planthood.settings')
 django.setup()
 
+from plantswap.models import Reminder
+
 sched = BlockingScheduler()
 
-
-@sched.scheduled_job('corn', day_of_week='Mon-Sat', hour=8)
+@sched.scheduled_job('cron', day_of_week='Mon-Sat', hour=8)
 def scheduled_job():
+    recipients = []
+
+    for reminder in Reminder.objects.all():
+        if reminder.next_care_day.today():
+            recipients.append(reminder.creator.email)
+
     with mail.get_connection() as connection:
         mail.EmailMessage(
-            'Your plants need your attention!', 'test treści',
+            'Twoje rośliny wymagają uwagi!',
+            'Zaloguj się do aplikacji, by sprawdzić przypomnienia!',
             'planthood.mokotow@gmail.com',
-            ['planthood.mokotow@gmail.com'],
+            recipients,
             connection=connection,
         ).send()
-    print('Reminders sent at 8am.')
+    print('Reminders sent at 6am.')
 
 
 sched.start()
