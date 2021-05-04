0a1d860ef80483fbfdc5d0b9ee38bc4b09e87bf1
megaz0rd <akropiewnicki@gmail.com>
2021-05-04 20:38:39 +0200

    heroku setts

--- clock.py
+++ clock.py
@@ -7,20 +7,20 @@
 
 from apscheduler.schedulers.blocking import BlockingScheduler
 
-# from django.core.mail import send_mail
+from django.core.mail import send_mail
 
 sched = BlockingScheduler()
 
 
 @sched.scheduled_job('interval', minutes=3)
 def scheduled_job():
-    # send_mail(
-    #     'test tematu',
-    #     'test treści',
-    #     'planthood-mokotow@gmail.com',
-    #     ['planthood-mokotow@gmail.com'],
-    #     fail_silently=False
-    # )
+    send_mail(
+        'test tematu',
+        'test treści',
+        'planthood.mokotow@gmail.com',
+        ['planthood.mokotow@gmail.com'],
+        fail_silently=False
+    )
     print('Reminders sent at 8am.')
 
 
