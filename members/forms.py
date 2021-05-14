d3a9a70fcb0f51b5098d8722055024a07eb0e857
megaz0rd <akropiewnicki@gmail.com>
2021-05-14 23:36:54 +0200

    Calculate distance/Implement google maps api

--- members/forms.py
+++ members/forms.py
@@ -1,16 +1,31 @@
 from django import forms
-from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.contrib.auth.forms import (
+    UserChangeForm,
+    UserCreationForm,
+    PasswordChangeForm
+)
 from django.core.exceptions import ValidationError
 
 from members.models import UserProfile, User
 
 
 class UserRegistrationForm(UserCreationForm):
-    email = forms.EmailField()
-    first_name = forms.CharField(max_length=64)
-    last_name = forms.CharField(max_length=64)
-    street = forms.CharField(max_length=64)
-    building_number = forms.CharField(max_length=8)
+    username = forms.CharField(max_length=150, label='Nazwa użytkownika')
+    email = forms.EmailField(label='Adres e-mail')
+    first_name = forms.CharField(max_length=64, label='Imię')
+    last_name = forms.CharField(max_length=64, label='Nazwisko')
+    street = forms.CharField(max_length=64, label='Ulica')
+    building_number = forms.CharField(max_length=8, label='Nr budynku')
+    password1 = forms.CharField(required=True, label='Nowe hasło',
+                                widget=forms.PasswordInput(
+                                    attrs={'class': 'form-control'}),
+                                error_messages={
+                                    'required': 'Hasło nie może być puste'})
+    password2 = forms.CharField(required=True, label='Powtórz nowe hasło',
+                                widget=forms.PasswordInput(
+                                    attrs={'class': 'form-control'}),
+                                error_messages={
+                                    'required': 'Hasło nie może być puste'})
 
     class Meta:
         model = User
@@ -26,14 +41,14 @@ class UserRegistrationForm(UserCreationForm):
     def clean(self):
         email = self.cleaned_data.get('email')
         if User.objects.filter(email=email).exists():
-            raise ValidationError("This email address is already registered.")
+            raise ValidationError("Konto o podanym adresie e-mail istnieje!")
         return self.cleaned_data
 
 
 class UserEditForm(UserChangeForm):
-    first_name = forms.CharField(max_length=64)
-    last_name = forms.CharField(max_length=64)
-    email = forms.EmailField()
+    first_name = forms.CharField(max_length=64, label='Imię')
+    last_name = forms.CharField(max_length=64, label='Nazwisko')
+    email = forms.EmailField(label='Adres e-mail')
     password = None
 
     class Meta:
@@ -46,10 +61,47 @@ class UserEditForm(UserChangeForm):
 
 
 class UserProfileEditForm(forms.ModelForm):
-
     class Meta:
         model = UserProfile
         fields = [
             'street',
             'building_number',
         ]
+        labels = {
+            'street': 'Ulica',
+            'building_number': 'Numer budynku',
+        }
+
+        widgets = {
+            'street': forms.TextInput(attrs={'placeholder': ''}),
+            'building_number': forms.TextInput(
+                attrs={'placeholder': ''}),
+        }
+
+
+class UserPasswordChangeForm(PasswordChangeForm):
+    error_messages = {'password_incorrect': 'Stare hasło jest niepoprawne'}
+    old_password = forms.CharField(required=True, label='Stare hasło',
+                                   widget=forms.PasswordInput(
+                                       attrs={'class': 'form-control'}),
+                                   error_messages={
+                                       'required': 'Hasło nie może być puste'})
+    new_password1 = forms.CharField(required=True, label='Nowe hasło',
+                                    widget=forms.PasswordInput(
+                                        attrs={'class': 'form-control'}),
+                                    error_messages={
+                                        'required': 'Hasło nie może być puste'})
+    new_password2 = forms.CharField(required=True, label='Powtórz nowe hasło',
+                                    widget=forms.PasswordInput(
+                                        attrs={'class': 'form-control'}),
+                                    error_messages={
+                                        'required': 'Hasło nie może być puste'})
+
+
+class Meta:
+    model = User
+    fields = [
+        'old_password',
+        'new_password1',
+        'new_password2'
+    ]
--- members/models.py
+++ members/models.py
@@ -1,5 +1,11 @@
+import requests
 from django.db import models
 from django.contrib.auth import get_user_model
+
+try:
+    from planthood.local_settings import GOOGLE_MAPS_API_KEY
+except ModuleNotFoundError:
+    pass
 
 User = get_user_model()
 
@@ -8,6 +14,27 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     street = models.CharField(max_length=64)
     building_number = models.CharField(max_length=8)
+    city = models.CharField(max_length=32, default='Warszawa')
+    latitude = models.DecimalField(max_digits=9, decimal_places=6,
+                                   blank=True, default=0)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6,
+                                    blank=True, default=0)
 
     def __str__(self):
         return self.user.email
+
+    def save(self, *args, **kwargs):
+        if GOOGLE_MAPS_API_KEY:
+            address = " ".join(
+                [self.street, self.building_number, self.city])
+            api_response = requests.get(
+                'https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}'.format(
+                    address, GOOGLE_MAPS_API_KEY))
+            api_response_dict = api_response.json()
+            if api_response_dict['status'] == 'OK':
+                self.latitude = api_response_dict['results'][0]['geometry']['location']['lat']
+                self.longitude = api_response_dict['results'][0]['geometry']['location']['lng']
+
+            super(UserProfile, self).save(*args, **kwargs)
+        else:
+            super(UserProfile, self).save(*args, **kwargs)
